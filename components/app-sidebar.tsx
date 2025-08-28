b6019ea47c49ad5495bf3aa3490f2b05fccdb71d
Sam Potter <spotter1642@gmail.com>
2025-08-28 09:30:57 -0400

    Add a basic sidebar

--- components/app-sidebar.tsx
+++ components/app-sidebar.tsx
@@ -1,9 +1,46 @@
-import { Sidebar, SidebarContent } from "@/components/ui/sidebar";
+import { Sidebar, SidebarContent, SidebarGroup, SidebarGroupContent, SidebarGroupLabel, SidebarMenu, SidebarMenuButton, SidebarMenuItem } from "@/components/ui/sidebar";
+import { Home, Landmark, Settings, User } from "lucide-react";
+import { AppSidebarGroup, AppSidebarGroupProps } from "./app-sidebar-group";
+
+const finaceMenuGroupProps: AppSidebarGroupProps = {
+  label: "Finances",
+  menuItems: [
+    {
+      title: "Home",
+      url: "#",
+      icon: Home
+    },
+    {
+      title: "Accounts",
+      url: "#",
+      icon: Landmark
+    }
+  ]
+}
+
+const userMenuGroupProps: AppSidebarGroupProps = {
+  label: "User",
+  menuItems: [
+    {
+      title: "Profile",
+      url: "#",
+      icon: User
+    },
+    {
+      title: "Settings",
+      url: "#",
+      icon: Settings
+    }
+  ]
+}
 
 export function AppSidebar() {
   return (
     <Sidebar>
-      <SidebarContent />
+      <SidebarContent>
+        <AppSidebarGroup {...finaceMenuGroupProps} />
+        <AppSidebarGroup {...userMenuGroupProps} />
+      </SidebarContent>
     </Sidebar>
   )
 }
