9eaef1b17f7d0a3e68307d22066c28a390b0e860
Sam Potter <spotter1642@gmail.com>
2025-09-12 21:52:41 -0400

    Give the header some room to breath

--- frontend/app/components/app-header.tsx
+++ frontend/app/components/app-header.tsx
@@ -4,8 +4,8 @@ import { SidebarTrigger } from "./ui/sidebar"
 
 export function SiteHeader() {
   return (
-    <header className="flex h-(--header-height) shrink-0 items-center gap-2 border-b transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-(--header-height)">
-      <div className="flex w-full items-center gap-1 px-4 lg:gap-2 lg:px-6">
+    <header className="flex h-20 shrink-0 items-center gap-2 border-b transition-[width,height] ease-linear">
+      <div className="flex w-full items-center gap-1 px-4 py-4 lg:gap-2 lg:px-6">
         <SidebarTrigger className="-ml-1" />
         <Separator
           orientation="vertical"
