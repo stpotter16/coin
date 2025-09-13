cb5cba73fdf1e20643f4b46001ef78606adfdbc1
Sam Potter <spotter1642@gmail.com>
2025-09-12 21:28:06 -0400

    Rename dashboard page title

--- frontend/app/routes/home.tsx
+++ frontend/app/routes/home.tsx
@@ -3,7 +3,7 @@ import { Dashboard } from "~/dashboard/dashboard";
 
 export function meta({ }: Route.MetaArgs) {
   return [
-    { title: "Coin" },
+    { title: "Coin - Dashboard" },
     { name: "description", content: "Coin dashboard" },
   ];
 }
