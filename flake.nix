5b691ae3ecd2518945d741ed847b23fad85b74b1
Sam Potter <spotter1642@gmail.com>
2025-12-30 21:02:57 -0500

    Add go tooling

--- flake.nix
+++ flake.nix
@@ -2,31 +2,78 @@
     description = "Dev environment for Coin";
 
     inputs = {
-        flake-utils.url = "github:numtide/flake-utils";
+      flake-utils.url = "github:numtide/flake-utils";
 
-        # 24.5.0
-        nodejs-nixpkgs.url = "github:NixOS/nixpkgs/281aac132f6cd84252a5a242cde14c183f600cbc";
+      # 1.25.2 release
+      go-nixpkgs.url = "github:NixOS/nixpkgs/01b6809f7f9d1183a2b3e081f0a1e6f8f415cb09";
+
+      # 3.50.4 release
+      sqlite-nixpkgs.url = "github:NixOS/nixpkgs/6f374686605df381de8541c072038472a5ea2e2d";
+
+      # 0.3.13 release
+      litestream-nixpkgs.url = "github:NixOS/nixpkgs/ee09932cedcef15aaf476f9343d1dea2cb77e261";
+
+      # 0.11.0 release
+      shellcheck-nixpkgs.url = "github:NixOS/nixpkgs/ee09932cedcef15aaf476f9343d1dea2cb77e261";
+
+      # 3.5.0 release
+      sqlfluff-nixpkgs.url = "github:NixOS/nixpkgs/ee09932cedcef15aaf476f9343d1dea2cb77e261";
+
+      # 24.5.0
+      nodejs-nixpkgs.url = "github:NixOS/nixpkgs/281aac132f6cd84252a5a242cde14c183f600cbc";
     };
 
     outputs = {
-        self,
-        flake-utils,
-        nodejs-nixpkgs
+      self,
+      flake-utils,
+      go-nixpkgs,
+      sqlite-nixpkgs,
+      litestream-nixpkgs,
+      shellcheck-nixpkgs,
+      sqlfluff-nixpkgs,
+      nodejs-nixpkgs
     } @inputs:
-        flake-utils.lib.eachDefaultSystem (system: let
-            nodejspkg = nodejs-nixpkgs.legacyPackages.${system};
-        in {
-            devShells.default = nodejspkg.mkShell {
-                packages = [
-                    nodejspkg.nodejs_24
-                ];
+      flake-utils.lib.eachDefaultSystem (system: let
+        gopkg = go-nixpkgs.legacyPackages.${system};
+        go = gopkg.go_1_25;
+        sqlite = sqlite-nixpkgs.legacyPackages.${system}.sqlite;
+        litestream = litestream-nixpkgs.legacyPackages.${system}.litestream;
+        shellcheck = shellcheck-nixpkgs.legacyPackages.${system}.shellcheck;
+        sqlfluff = sqlfluff-nixpkgs.legacyPackages.${system}.sqlfluff;
+        nodejs = nodejs-nixpkgs.legacyPackages.${system}.nodejs_24;
+      in {
+        devShells.default = gopkg.mkShell {
+            packages = [
+              gopkg.gotools
+              gopkg.gopls
+              gopkg.go-outline
+              gopkg.gopkgs
+              gopkg.gocode-gomod
+              gopkg.godef
+              gopkg.golint
+              go
+              sqlite
+              litestream
+              shellcheck
+              sqlfluff
+              nodejs
+            ];
 
-                shellHook = ''
-                    echo "node" "$(node --version)"
-                    echo "npm" "$(npm --version)"
-                    echo "npx" "$(npx --version)"
-                '';
-            };
-        });
+            shellHook = ''
+              PROJECT_NAME="$(basename "$PWD")"
+              export GOPATH="$HOME/.local/share/go-workspaces/$PROJECT_NAME"
+              export GOROOT="${go}/share/go"
+
+              go version
+              echo "node" "$(node --version)"
+              echo "npm" "$(npm --version)"
+              echo "npx" "$(npx --version)"
+              echo "sqlite" "$(sqlite3 --version | cut -d ' ' -f 1-2)"
+              echo "litestream" "$(litestream version)"
+              echo "shellcheck" "$(shellcheck --version | grep '^version:')"
+              sqlfluff --version
+            '';
+        };
+      });
 }
 
