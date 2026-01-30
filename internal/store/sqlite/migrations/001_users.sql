4bea1a551fbad78354c6a78fb856f4e754f092c7
Sam Potter <spotter1642@gmail.com>
2026-01-30 12:25:21 -0500

    Flesh out user and session table

--- internal/store/sqlite/migrations/001_users.sql
+++ internal/store/sqlite/migrations/001_users.sql
@@ -1,3 +1,14 @@
-CREATE TABLE IF NOT EXISTS users (
-    id INTEGER PRIMARY KEY
+CREATE TABLE IF NOT EXISTS user (
+    id INTEGER PRIMARY KEY,
+    username TEXT NOT NULL UNIQUE,
+    password TEXT NOT NULL,
+    is_admin INTEGER NOT NULL,
+    created_time TEXT NOT NULL,
+    last_modified_time TEXT NOT NULL
+) STRICT;
+
+CREATE TABLE IF NOT EXISTS session (
+    session_key TEXT PRIMARY KEY,
+    value BLOB,
+    expires_at TEXT NOT NULL
 ) STRICT;
